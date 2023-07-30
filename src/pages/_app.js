60eec3d6465ac18aab3918de1c87eaba529e7f43
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-30 10:33:31 +0600

    [DONE] redux features

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import '@/styles/globals.css'
 import { Provider } from 'react-redux'
 import { SessionProvider } from "next-auth/react"
+import store from '@/redux/store'
 
 export default function App({ Component, pageProps }) {
+// user the Layout define at the page Level, if available
+  const getLayout = Component.getLayout || ((page) => page)
   return (<Provider store={store}>
 
     <SessionProvider session={pageProps.session}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./features/product/productSlice";
+import userReducer from "./features/user/userSlice";
+import productCategoryReducer from "./features/productCategory/productCategorySlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user : userReducer,
+    products : productReducer,
+    productsCategory : productCategoryReducer,
+    
+  },
 });
 
 export default store;
