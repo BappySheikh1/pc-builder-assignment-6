16cf9fd39e2ff0b4dbb575399928bf52b1d90e45
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-30 10:08:43 +0600

    [DONE]nextauth setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Provider } from 'react-redux'
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<Provider store={store}>
+
+    <SessionProvider session={pageProps.session}>
+      {getLayout(
+        <Component {...pageProps} />
+      )}
+    </SessionProvider>
+  </Provider>
+  )
+
 }
