56eda49eaa1523a30fad0ba15a5e05324ab162d5
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-30 13:54:08 +0600

    product details added

--- src/pages/detail/[productId].js
+++ src/pages/detail/[productId].js
@@ -1,10 +1,38 @@
+import ProductCardDetails from "@/component/CardDetails";
+import RootLayout from "@/component/Layouts/RootLayout";
+import { useRouter } from "next/router";
 
-const ProductDetailPage = () => {
-    return (
-        <div>
-            <h1 className='text-5xl text-center' >This is Product Detail Page</h1>
-        </div>
-    );
+const ProductDetail = ({ product }) => {
+  const router = useRouter();
+  const productDetails = product.data;
+  return (
+    <div>
+      <ProductCardDetails productDetails={productDetails} />
+    </div>
+  );
 };
 
-export default ProductDetailPage;
+export default ProductDetail;
+
+ProductDetail.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export async function getStaticPaths() {
+  const res = await fetch(`https://pc-bd.vercel.app/api/v1/product`);
+  const products = await res.json();
+
+  const paths = products.data.map((product) => ({
+    params: { productId: product._id },
+  }));
+  return { paths, fallback: false };
+}
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const res = await fetch(
+    `https://pc-bd.vercel.app/api/v1/product/${params.pronactId}`
+  );
+  const data = await res.json();
+  return { props: { product: data } };
+};
--- src/pages/product/[catagory].js
+++ src/pages/product/[catagory].js
@@ -1,11 +1,35 @@
-
+import ProductsCard from "@/component/Card";
+import RootLayout from "@/component/Layouts/RootLayout";
+import { useAppSelector } from "@/redux/hook";
+import { useReducer } from "react";
 
 const ProductCategory = () => {
-    return (
-        <div>
-            <h1 className='text-6xl text-center '>This is Product Category page</h1>
-        </div>
-    );
+  const { products } = useAppSelector((state) => state.ProductCategory);
+  const router = useReducer();
+
+  const filteredProducts = products.filter(
+    (product) => product.category === `${router.query.category}`
+  );
+
+  console.log(filteredProducts);
+
+  return (
+    <div className="container mx-auto my-14">
+      <div className="text-center">
+        <h1 className="text-4xl my-3">Featured Products</h1>
+        <p>Check & Get Your Desired Product!</p>
+      </div>
+      <div className="grid  grid-cols-1 md:grid-cols-5 gap-4 justify-items-center my-10">
+        {filteredProducts.map((product) => (
+          <ProductsCard product={product} key={product._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductCategory;
+
+ProductCategory.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
