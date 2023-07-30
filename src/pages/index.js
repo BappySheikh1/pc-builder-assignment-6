83828014e6d918e6c7a31ecf91cc1e67c6c50ca8
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-30 11:02:21 +0600

    home page getProducts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
+const { default: RootLayout } = require("@/component/Layouts/RootLayout");
 
-
-export default function Home() {
+const HomePage = ({ products }) => {
+  console.log(products);
   return (
     <>
-    <h1 className='text-3xl font-bold'>This is assignment 6 project</h1>
+      <h1 className="text-3xl font-bold">This is assignment 6 project</h1>
     </>
-  )
-}
+  );
+};
+
+export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://pc-bd.vercel.app/api/v1/product/?limit=100`);
+  const data = await res.json();
+  return {
+    props: {
+      products: data,
+    },
+    revalidate: 10,
+  };
+};
