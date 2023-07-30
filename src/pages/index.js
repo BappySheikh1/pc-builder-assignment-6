8f6e3eeb5b100dbd5857c78f0eca62dba5715211
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-30 11:23:22 +0600

    home page show random card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
+import ProductsCard from "@/component/Card";
+import { getProductsCatagoriSuccess } from "@/redux/features/productCategory/productCategorySlice";
+import { useAppDispatch } from "@/redux/hook";
+import { useEffect } from "react";
+
 const { default: RootLayout } = require("@/component/Layouts/RootLayout");
 
 const HomePage = ({ products }) => {
-  console.log(products);
+  // console.log(products);
+  const randomSixProducts = products?.data.slice(0, 6);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (products) {
+      dispatch(getProductsCatagoriSuccess(products.data));
+    }
+  }, [dispatch, products]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold">This is assignment 6 project</h1>
+      <div className="container mx-auto my-14">
+        <div className="text-center">
+          <h1 className="text-4xl my-3">Featured Products</h1>
+          <p>Check & Get Your Desired Product!</p>
+        </div>
+
+        <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5  justify-items-center my-10">
+          {randomSixProducts.map((product) => (
+            <ProductsCard product={product} key={product._id} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
