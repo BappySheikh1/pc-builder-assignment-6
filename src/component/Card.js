3d7e3d3d667ed8892d2fea94b654ac4113d15573
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-30 10:43:30 +0600

    [DONE] rootLayout

---
+++ src/component/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsCard = () => {
+    return (
+        <div>
+            <h1>This is cardDetails Page</h1>
+        </div>
+    );
+};
+
+export default ProductsCard;
